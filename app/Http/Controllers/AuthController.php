40a5693c8e45eb0209bd34b98b404a9cdb05b1fb
Daniel Alvarez <ingenierodaniel7@gmail.com>
2022-03-27 10:21:52 -0600

    login y logout ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8'
+    ]);
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken('auth_tokens')->plainTextToken;
+
+        return response()
+            ->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer', ]);
+    }
+    public function login(Request $request){
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message' => 'Unauthorized'],401);
+        }
+        $user = User::where('email',$request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Hi '.$user->name,
+            'accessToken' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'You have successfully logged out an the token was successfully deleted'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('v1/character', App\Http\Controllers\api\v1\CharacterController::class);
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('logout',[AuthController::class,'logout']);
 });
